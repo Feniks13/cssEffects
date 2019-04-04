6693fbab7bd0008577bbf70e2762aa8a168751f2
Sergey Privalov <kalabanga12@yandex.ru>
2019-04-04 15:57:19 +0500

    Добавлен блок

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  var link = $('.menu-link');
+  var menu = $('.menu');
+  var nav_link = $('.menu a')
+  
+  link.click(function(){
+    link.toggleClass('menu-link_active');
+    menu.toggleClass('menu_active');
+  });
+  nav_link.click(function(){
+    link.toggleClass('menu-link_active');
+    menu.toggleClass('menu_active');
+  })
+});
